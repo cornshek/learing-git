3e09074b68847ab4e144ed8f903c9f1804ddfd27
cornshek <616615110@qq.com>
2020-02-23 21:08:35 +0800

    'commitagain'

--- src/io/AutoCreate.java
+++ src/io/AutoCreate.java
@@ -28,6 +28,7 @@ public class AutoCreate {
         }
 
         //从控制台获取所需参数
+        //我要试试添加提交信息
         System.out.println("请输入类的名称");
         String classname = s.nextLine();
         System.out.println("请输入属性的类型");
