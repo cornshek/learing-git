989685342eef2cc701017449a70b4e2c75afb2a7
cornshek <616615110@qq.com>
2020-03-03 23:46:05 +0800

    练习jdbc

--- src/Test.java
+++ src/Test.java
@@ -5,12 +5,13 @@ import collection.LearnHashMap;
 import collection.LearnLinkedList;
 import collection.binarytree.HeroNode;
 import collection.binarytree.Node;
+import jdbc.LearnJDBC;
 
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        LearnComparator.run();
+        LearnJDBC.run();
     }
 
 }
---
+++ src/collection/LearnAggregate.java
@@ -0,0 +1,5 @@
+package collection;
+
+//TODO 什么破玩意暂时不想学
+public class LearnAggregate {
+}
